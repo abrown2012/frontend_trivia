8675ddcadeb105f1d4ed07ba8eabaec37ec1c6da
Agnieszka Brown <abrown2012@icloud.com>
2021-06-06 14:22:06 -0400

    changes the front page after user submits name

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,13 @@
 const buttonStartTrivia = () => document.getElementById("start-trivia")
 const ul = () => document.getElementById("questions-list")
+const userName = () => document.getElementById("user-name")
+const pageTitle = () => document.getElementById("page-title")
+const subtitle = () => document.getElementById("subtitle")
+const nameForm = () => document.getElementById("name-form")
+
+
 document.addEventListener("DOMContentLoaded", () =>{
-    buttonStartTrivia().addEventListener("click", handleClick)
+    buttonStartTrivia().addEventListener("click", handleSubmit)
 })
 
 const handleClick = () => {
@@ -24,4 +30,35 @@ const renderQuestions = (questions) => {
         ul().appendChild(li)
     })
 
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = {
+        name: userName().value
+    }
+    fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(resp => resp.json())
+    .then(json => handleCreateUser(json.name))
+}
+
+const handleCreateUser = (name) => {
+    userName().value = ""
+    pageTitle().innerText = `Welcome, ${name}!`
+    subtitle().innerText = "Let's play a game of Trivia"
+    nameForm().innerHTML = ""
+    handleCreateQuiz()
+}
+
+const handleCreateQuiz = () => {
+    fetch('http://localhost:3000/questions')
+    .then(resp => resp.json())
+    .then(json => {debugger})
+    
 }
