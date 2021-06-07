b0a2989d34bdf44f2a0d9880870d2246d24e3528
Agnieszka Brown <abrown2012@icloud.com>
2021-06-07 15:56:37 -0400

    changes the page formating after clicking the start button

--- javascripts/index.js
+++ javascripts/index.js
@@ -4,33 +4,12 @@ const userName = () => document.getElementById("user-name")
 const pageTitle = () => document.getElementById("page-title")
 const subtitle = () => document.getElementById("subtitle")
 const nameForm = () => document.getElementById("name-form")
+const questionText = () => document.getElementById("question-container")
 
 
 document.addEventListener("DOMContentLoaded", () =>{
     buttonStartTrivia().addEventListener("click", handleSubmit)
 })
-
-const handleClick = () => {
-    fetch('http://localhost:3000/questions')
-    .then(resp => resp.json())
-    .then(json => renderQuestions(json))
-    .catch(handleError)
-}
-
-const handleError = (error) => {
-    console.log(error)
-}
-
-const renderQuestions = (questions) => {
-    questions.forEach(element =>{
-        const li = document.createElement('li')
-        li.innerHTML = `
-        <h2 class="question-text">${element.text}</h2>
-        `
-        ul().appendChild(li)
-    })
-
-}
 
 const handleSubmit = (e) => {
     e.preventDefault()
@@ -49,16 +28,70 @@ const handleSubmit = (e) => {
 }
 
 const handleCreateUser = (name) => {
-    userName().value = ""
     pageTitle().innerText = `Welcome, ${name}!`
     subtitle().innerText = "Let's play a game of Trivia"
     nameForm().innerHTML = ""
     handleCreateQuiz()
+    handleStartQuiz()
 }
 
 const handleCreateQuiz = () => {
     fetch('http://localhost:3000/questions')
     .then(resp => resp.json())
-    .then(json => {debugger})
+    .then(json => select10Qustions(json))
+}
+
+const select10Qustions = (json) => {
+    let newJson = json 
+    let newObject = []
+    for (i = 10; i > 0; i--) {
+    item = parseInt(Math.random() * (newJson.length - 2) )
+    newObject.push(newJson[item])
+    newJson.splice([item], 1)
+    }
+    createQuiz(newObject)
+}
+
+const createQuiz = (randomQuestions) => {
+    fetch('http://localhost:3000/users')
+    .then(resp => resp.json())
+    .then(json => currentUserID(json, randomQuestions))
     
+}
+
+const currentUserID = (json, randomQuestions) => {
+    const data = {
+        user_id: json[json.length-1].id
+    }
+    debugger 
+    fetch("http://localhost:3000/quizzes", {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(resp => resp.json())
+    .then(json => addQuestions(json, randomQuestions))
+}
+
+const addQuestions = (json, questions) => {
+    questions.forEach(question => {
+        const data = {
+            question_id: question.id,
+            quiz_id: json.id 
+        }
+        fetch(`http://localhost:3000/quiz_questions`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    })
+}
+
+const handleStartQuiz = () => {
+    debugger
+    console.log("Your trivia just started")
 }
